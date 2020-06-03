e87d2b5f4171a804e03cefe78eabbcbe13a11ae0
Ahmad Jarara <ajarara94@gmail.com>
2020-06-03 18:21:59 -0400

    propagate network dependencies down to lib

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -13,6 +13,11 @@ tasks.withType(Test::class.java) {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("com.squareup.retrofit2:retrofit:2.7.1")
+    implementation("com.squareup.retrofit2:adapter-rxjava2:2.7.1")
+    implementation("com.squareup.retrofit2:converter-jackson:2.7.1")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.10")
+
     testImplementation("io.kotest:kotest-runner-junit5-jvm:4.0.6")
     testImplementation("io.kotest:kotest-assertions-core-jvm:4.0.6")
 }
